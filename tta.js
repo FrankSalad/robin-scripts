7df3a9e61eaf3e29f0fbc6dd224231636d9b8ced
Francisco Saldana <francisco@ginger.io>
2016-04-01 21:29:42 -0500

    Add global, append.

--- tta.js
+++ tta.js
@@ -12,14 +12,15 @@
 
 (function() {
     'use strict';
-
+    clearInterval(window.tapToAt);
     var chatinput = $('#robinChatInput input[type="text"]');
-    setInterval(function(){
+    window.tapToAt = setInterval(function(){
         var a=$('.robin--username').not('.tap-to-at');
         a.addClass('tap-to-at');
         a.click(function(){
             var u=$(this).text();
-            chatinput.val('@' + u);
+            chatinput.val(chatinput.val() + '@' + u + ', ');
+            chatinput.focus();
         });
     }, 100);
 })();
